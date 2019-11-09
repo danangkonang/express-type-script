1aaa2f89a4642963ad1fbe3f777511572acf4e98
danangkonang <52842458+danangkonang@users.noreply.github.com>
2019-11-10 00:26:44 +0700

    psg

--- router.js
+++ router.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(bodyParser.json())
 
 const controlerProduct = require('./controlers/products')
+
 app.get('/', (req, res) => res.send('Hello danang!'))
 app.get('/products',controlerProduct.index)
 app.get('/product/:id_barang',controlerProduct.show)
