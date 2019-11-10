65c924501ce073cc82a2304a139365fd2d8942dd
danangkonang <danang@localhost.localdomain>
2019-11-10 10:00:40 +0700

    error fix

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const port = process.env.PORT || 5000
 const app = require('./router')
 app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+// "test": "echo \"Error: no test specified\" && exit 1",
