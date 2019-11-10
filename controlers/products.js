c1fbc164b54866e3f6ee032a424263b591d40aae
danangkonang <danang@localhost.localdomain>
2019-11-10 09:21:07 +0700

    pusing

--- controlers/products.js
+++ controlers/products.js
@@ -5,7 +5,8 @@ exports.index=(req,res)=>{
         res.send(results)
     })
 }
-// npx sequelize-cli model:generate --name User --attributes firstName:string,lastName:string,email:string
+
+
 exports.show=(req,res)=>{
     const id = req.params.id_barang
     conn.query('SELECT * FROM `barang` WHERE `id_barang`= ?', [id],(err,results)=>{
